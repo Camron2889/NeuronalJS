ee2ff5f284fd8c6b6d473521b00c2c7333325daf
Camron2889 <Camron2889@gmail.com>
2018-12-07 22:21:51 -0500

    Create template.js

---
+++ notes/template.js
@@ -0,0 +1,20 @@
+//namespace
+this.neuronal = this.neuronal || {};
+
+//class
+(function() {
+    "use strict";
+    
+    //constructor
+    const ClassName = function() {
+        
+    };
+    
+    const proto = ClassName.prototype;
+    
+    proto.methodName = function() {
+        
+    };
+    
+    neuronal.ClassName = ClassName;
+})();
