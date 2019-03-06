abf18086ca8d766c7309da601dbd52e582e07552
Camron2889 <Camron2889@gmail.com>
2019-03-06 14:42:28 -0500

    Update template.js

--- notes/template.js
+++ notes/template.js
@@ -1,20 +1,37 @@
 //namespace
-this.neuronal = this.neuronal || {};
+this.namespaceName = this.namespaceName || {};
 
-//class
+//class enclosure
 (function() {
     "use strict";
     
     //constructor
     const ClassName = function() {
-        
+
+        //public non-static
+        this.methodName = function() { 
+          //It's rarely necessary for a method to be non-static.
+        };
+
+        this.propertyName = "value";
     };
-    
+
     const proto = ClassName.prototype;
-    
+
+    //public static
     proto.methodName = function() {
         
     };
-    
-    neuronal.ClassName = ClassName;
+
+    proto.propertyName = "value";
+
+    //private
+    const _methodName = function() {
+
+    };
+
+    const _propertyName = "value";
+
+    //attach class to namespace
+    namespaceName.ClassName = ClassName;
 })();
