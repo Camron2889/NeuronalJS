f0dbc1f383402503abf91735f21868bdb171d7a5
Camron2889 <Camron2889@gmail.com>
2019-06-26 10:41:48 -0400

    Create fhn-node.js

---
+++ fhn-node.js
@@ -0,0 +1,52 @@
+//namespace
+this.neuronal = this.neuronal || {};
+
+//class
+(function() {
+    "use strict";
+    
+    //private static data
+    const _defaults = {
+        gNa: 0.9,
+        gK: 1.1,
+        beta: 0.6,
+        gamma: 1,
+        c: 0.025,
+
+        deltaT: 2 / 256,
+
+        uDefault: -0.53,
+        vDefault: -1.13 //should be resting voltage
+    };
+    
+    //constructor
+    const FhnNode = function() {
+        this.restoreDefaults();
+        this.reset();
+    };
+    
+    const proto = FhnNode.prototype;
+    
+    //public methods
+    proto.restoreDefaults = function() {
+        const keys = Object.keys(_defaults);
+        for (let i = 0; i < keys.length; i++) {
+            this[keys[i]] = _defaults[keys[i]];
+        }
+    };
+    
+    proto.reset = function() {
+        this.v = this.vDefault;
+        this.u = this.uDefault;
+    };
+    
+    proto.calculate = function() {
+        const f = this.v * (1 - (Math.pow(this.v, 2) / 3));
+        const newU = (this.v + this.beta - this.gamma * this.u) * this.deltaT + this.u;
+        const newV = 1 / this.c * (this.gNa * this.f - this.gK * this.u) * this.deltaT + this.v;
+        this.u = newU;
+        this.v = newV;
+    };
+    
+    neuronal.FhnNode = FhnNode;
+})();
